c939f318a2b87c8726310522c498f8ad28905f96
Michael <michael.shihrer@gmail.com>
2015-02-22 22:51:56 -0700

    Implements location listener interface

--- AndroidLocation/MainActivity.cs
+++ AndroidLocation/MainActivity.cs
@@ -5,17 +5,21 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Locations;
 
 namespace AndroidLocation
 {
     [Activity(Label = "AndroidLocation", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, ILocationListener
     {
-        int count = 1;
+        LocationManager locMgr;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
+            //Initialize location manager
+            locMgr = GetSystemService(Context.LocationService) as LocationManager;
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
@@ -23,6 +27,43 @@ namespace AndroidLocation
             // Get our button from the layout resource,
             // and attach an event to it
 
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            string provider = LocationManager.GpsProvider;
+
+            if (locMgr.IsProviderEnabled(provider))
+            {
+                locMgr.RequestLocationUpdates(provider, 2000, 1, this);
+            }
+        }
+
+        private void getLocation()
+        {
+
+        }
+
+        public void OnLocationChanged(Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        {
+            throw new NotImplementedException();
         }
     }
 }
